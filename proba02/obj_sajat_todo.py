dc5099a6ca20cd647c486bd56f6de7e93f8bc948
Kincses László <kincsesster@gmail.com>
2021-07-21 19:11:25 +0200

    Még egy pici javítás a todóban.

--- proba02/obj_sajat_todo.py
+++ proba02/obj_sajat_todo.py
@@ -12,12 +12,21 @@ driver.get("https://gentle-bay-0e4e13803.azurestaticapps.net/todo.html")
 
 class Todooldal:
     def __init__(self):
+        self.beolvasás()
+
+    def beolvasás(self):
         self.tartalmazó_konténer = driver.find_element_by_xpath("/html/body/div/div/div")
         self.teendőlista_ul = self.tartalmazó_konténer.find_element_by_xpath("//ul[@class = 'unstyled']")
         # A todok listája, egyelőre nem használom.
         self.teendőlista = self.teendőlista_ul.find_elements_by_xpath("//li[@class = 'ng-scope']")
         # A meglevő todok, elsőre 5
         self.a_teendők_száma = self.tartalmazó_konténer.find_element_by_xpath("//span['@class = ng-binding']")
+        self.archive_link = self.tartalmazó_konténer.find_element_by_xpath("/html/body/div/div/div/a")
+        self.új_todószöveg = self.tartalmazó_konténer.find_element_by_xpath("/html/body/div/div/div/form/input[1]")
+        self.addgomb = self.tartalmazó_konténer.find_element_by_xpath("/html/body/div/div/div/form/input[2]")
+
+    def teardown(self):  # Lerombolás.
+        self.driver.close()
 
     def mennyi(self):
         self.a = self.a_teendők_száma.text.find(" ")  # Megkeresi az első szóközt az x of y... mezőben
@@ -32,18 +41,37 @@ class Todooldal:
         s = ""
         for i in range(a + 3, b):
             s += self.a_teendők_száma.text[i]
-        return s # visszaadja y-t.
+        return s  # visszaadja y-t.
 
     def rákatt(self, x):
         self.teendőlista[x].find_element_by_tag_name("input").click()
         # time.sleep(2)
 
+    def listát_frissít(self):
+        self.archive_link.click()
+        # Újra feltérképezi az oldalt.
+        self.beolvasás()
+
+    def új_todo(self, szöveg):
+        self.új_todószöveg.send_keys(szöveg)
+        self.addgomb.click()
+        self.beolvasás()
+
 
 oldal = Todooldal()
 
+oldal.rákatt(2)
+oldal.rákatt(0)
+oldal.rákatt(2)
+oldal.listát_frissít()
+oldal.új_todo("Új feladat.")
+oldal.új_todo("Még egy.")
+oldal.új_todo("Meg egy harmadik.")
+time.sleep(2)
+
 számlista = []  # Véletlenszámlista lesz.
 for i in range(100):
-    a = random.randint(0, 4)  # Véletlenszám 0-4 között, erre az elelmre kattintok.
+    a = random.randint(0, len(oldal.teendőlista) - 1)  # Véletlenszám 0-utolsó között, erre az elelmre kattintok.
     számlista.append(a)
     oldal.rákatt(a)  # Rákattint az a-adik listaelemre.
 
@@ -62,13 +90,4 @@ if int(oldal.mennyi()) == kipipálatlan:  # Ha stimmel, akkor "minden oké".
 else:
     print("Valami nem oké.")
 
-"""
-print("Mennyi:", oldal.mennyi())
-print("Összesen:", oldal.összes())
-oldal.rákatt(2)
-oldal.rákatt(3)
-oldal.rákatt(2)
-print("Utána mennyi:", oldal.mennyi())
-print("Utána összesen:", oldal.összes())
-"""
 driver.close()
